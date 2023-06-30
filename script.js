2e94eb889b4f4815cfd30acd89e79b6fd9d52cf1
Samal K <safty@mail.ru>
2023-06-30 11:56:05 +0600

    Commit for first task

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+let lang = 'ru' && 'eng';
+
+switch (true) {
+    case (lang = 'ru'):
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break
+    case (lang = 'eng'):
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break
+    default:
+        console.log('Все неверно');
+}
 
 
